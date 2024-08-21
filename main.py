26afef1792dee31952ad650b6a986ef02c6f7660
codedreammer <nkanal38@gmail.com>
2024-08-21 06:23:28 +0530

    add the code

---
+++ main.py
@@ -0,0 +1,22 @@
+print('Guess the number between 1 and 10. You have 3 attempts.') 
+ 
+num = 8
+guess = 0
+guess_limit=5
+guess_number = 0
+guess = int(input(f'Enter the Guess: '))
+guess_number +=1
+while guess_number < guess_limit:
+    
+    if guess != num:
+        guess_number +=1
+        if guess > num:
+            guess = int(input(f'{guess} Too high - Guess again 1-10: '))
+        else:
+            guess = int(input(f'{guess} too low - Guess again 1-10: '))
+    if guess == num:
+        print(f'You Win! You Guessed it: {guess}')
+        break
+    
+if guess != num:
+    print(f'Sorry you lose! It was {num}')
